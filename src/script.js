8392e7eb9a833c21b9bd3e8d88aebeb0b97ec31e
noozecrypt <noozecrypt@criptext.com>
2024-01-14 22:58:05 +0530

    promisiying setTimeout()

--- src/script.js
+++ src/script.js
@@ -1,10 +1,20 @@
 'use strict';
 
-console.log('Test Start');
-setTimeout(() => console.log('0 sec timer'), 0);
-Promise.resolve('Resolved Promise #1').then(res => console.log(res));
-Promise.resolve('Resolved promise #2').then(res => {
-    for (let i = 0; i < 1000000000; i++) { }
-    console.log(res);
-});
-console.log('Test End');
+const wait = function (delayInSeconds) {
+    return new Promise((resolve) => setTimeout(resolve, delayInSeconds * 1000));
+}
+
+wait(1)
+    .then(() => {
+        console.log('1 second passed');
+        return wait(1);
+    })
+    .then(() => {
+        console.log('2 seconds passed');
+        return wait(1);
+    })
+    .then(() => {
+        console.log('3 seconds passed');
+        return wait(1);
+    })
+    .then(() => console.log('4 seconds passed'));
