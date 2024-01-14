995395902f345e4147e12951d1447cff3707ffca
noozecrypt <noozecrypt@criptext.com>
2024-01-14 21:36:27 +0530

    initial commit
    Demo of MicroTasks Queue having higher precedence than the Callbacks Queue

--- src/script.js
+++ src/script.js
@@ -1,55 +1,10 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
-
-const renderCountry = function (data, className = '') {
-
-    const html = `
-    <article class="country ${className}">
-        <img class="country__img" src="${data.flags.png}" />
-        <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} Million</p>
-            <p class="country__row"><span>🗣️</span>${JSON.stringify(data.languages)}</p>
-            <p class="country__row"><span>💰</span>${JSON.stringify(data.currencies)}</p>
-        </div>
-    </article>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-};
-
-const getCountryAndNeighbor = function (countryCode) {
-
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/alpha/${countryCode}`);
-    request.send();
-
-    request.addEventListener('load', function () {
-
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
-        renderCountry(data);
-        const [neighbor] = data.borders;
-        if (!neighbor) return;
-
-        const request = new XMLHttpRequest();
-        request.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
-        request.send();
-
-        request.addEventListener('load', function () {
-            const [data] = JSON.parse(this.responseText);
-            console.log(data);
-            renderCountry(data, 'neighbour');
-        });
-
-    });
-};
-
-// getCountryAndNeighbor('in');
-getCountryAndNeighbor('us');
-
-
-///////////////////////////////////////
+console.log('Test Start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved Promise #1').then(res => console.log(res));
+Promise.resolve('Resolved promise #2').then(res => {
+    for (let i = 0; i < 1000000000; i++) { }
+    console.log(res);
+});
+console.log('Test End');
